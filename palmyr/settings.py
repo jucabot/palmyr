46eddfbf5f8e25c918debd4bc48d9b4edaa147af
jucabot <julien.cabot@yahoo.fr>
2013-03-18 11:36:19 +0100

    html template and static resources refactoring

--- palmyr/settings.py
+++ palmyr/settings.py
@@ -72,7 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/predictiveds/git/palmyr/palmyr/web/static',
+    '/home/predictiveds/git/palmyrjs/palmyrjs/web/static',
 )
 
 # List of finder classes that know how to find static files in
@@ -107,7 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/predictiveds/git/palmyr/palmyr/web/templates',
+    '/home/predictiveds/git/palmyrjs/palmyrjs/web/templates',
 )
 
 INSTALLED_APPS = (
